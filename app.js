75ba8497032c75104cbfdc4fa61214789e3d3fab
グレートサイヤマン8号 <105696434+nnhrBelievers@users.noreply.github.com>
2022-06-07 13:15:57 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -4,27 +4,8 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs')
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.render('login.ejs',{err:""});
-});
 
-app.use("/",require("./control/login.js"));
-
-app.post("/ok", (req,res)=>{
-  id =  req.body.username;
-  pass =  req.body.password;
-
-  if(id===req.body.username && pass===req.body.password){
-    const name = req.body.username
-
-    const data = {
-      name: name
-    }
-    res.render('ok.ejs', data)
-  }else{
-    res.render('login.ejs',{err:'打ち直し'});
-  }
-});
+app.use("/",require("./controller/login.js"));
 
 app.listen(3000, () => {
   console.log('これ動いた？')
